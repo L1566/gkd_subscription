816448cd7a3e0e1341d2d1a29c925470eddafb98
L1566 <185537890@qq.com>
2026-04-09 10:14:20 +0800

    新增QQ规则

--- src/apps/com.tencent.mobileqq.ts
+++ src/apps/com.tencent.mobileqq.ts
@@ -3,5 +3,21 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'com.tencent.mobileqq',
   name: 'QQ',
-  groups: [],
+  groups: [
+    {
+      key: 0,
+      name: '全屏广告-小程序广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          activityIds:
+            'com.tencent.luggage.wxa.standalone_open_runtime.container.WxaContainerMultiTaskActivity0',
+          matches: [
+            '@ImageView < FrameLayout <2 FrameLayout <5 FrameLayout <2 FrameLayout <2 FrameLayout < ViewGroup <2 FrameLayout < FrameLayout < [id="android:id/content"]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/i/26586330',
+        },
+      ],
+    },
+  ],
 });
